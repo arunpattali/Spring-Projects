4f16130c733324532dab22987099b9cacbaf6894
Cini <Cini@Lenovo-PC>
2019-05-21 20:58:50 +0530

    initial pull

---
+++ Spring-boot-Oauth2-login-demo/src/main/java/com/pk/example/SpringbootOauth2logindemo/SpringBootOauth2LoginDemoApplication.java
@@ -0,0 +1,13 @@
+package com.pk.example.SpringbootOauth2logindemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOauth2LoginDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOauth2LoginDemoApplication.class, args);
+	}
+
+}
